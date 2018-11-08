b764a9eda0196b66966d8f956e20274567e20b2e
hupeng2966 <417462204@qq.com>
2018-11-08 22:44:50 +0800

    2018-11-8 Patch Bug

--- foyoedu-consumer/src/main/java/com/foyoedu/Consumer_App.java
+++ foyoedu-consumer/src/main/java/com/foyoedu/Consumer_App.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ServletComponentScan
 @EnableEurekaClient
 @EnableFeignClients(basePackages = {"com.foyoedu.common.service"})
-@ComponentScan(value = "com.foyoedu")
+//@ComponentScan(value = "com.foyoedu")
 public class Consumer_App {
     public static void main(String[] args) {
         SpringApplication.run(Consumer_App.class, args);
